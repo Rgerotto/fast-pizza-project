09ebef1975ac7034b59aa37ec05d85caa6af830d
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-11-01 19:16:18 +0100

    tailwind done

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom"
 
 function Button({children, disabled, to, type}) {
     
-    const base = 'disabled:cursor-not-allowed bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2';
+    const base = 'text-sm disabled:cursor-not-allowed bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2';
     const styles = {
         
         primary: base + ' md:px-6 md:py-4 py-3 px-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
-        secondary: 'md:px-6 md:py-3.5 py-2.5 px-4 disabled:cursor-not-allowed border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2',
+        secondary: 'text-sm md:px-6 md:py-3.5 py-2.5 px-4 disabled:cursor-not-allowed border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2',
     }
     if(to){
     return <Link to={to} className={styles[type]}>{children}</Link>
