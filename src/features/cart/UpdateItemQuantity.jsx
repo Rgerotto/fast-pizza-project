56ec6d9b743f0ebb6c15da8205cee1efbde3b10d
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-11-03 20:43:38 +0100

    update shopping cart

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,16 @@
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button"
+import { decreaseItem, increaseItem } from "./cartSlice";
+
+function UpdateItemQuantity({pizzaId, currentQuantity}) {
+    const dispatch = useDispatch()
+    return (
+        <div className="flex gap-3 items-center md:gap-5">
+            <Button type='round' onClick={() => dispatch(decreaseItem(pizzaId))}>-</Button>
+            <span >{currentQuantity}</span>
+            <Button type='round' onClick={() => dispatch(increaseItem(pizzaId))}>+</Button>
+        </div>
+    )
+}
+
+export default UpdateItemQuantity;
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -7,6 +7,7 @@ function Button({children, disabled, to, type, onClick }) {
         
         primary: base + ' md:px-6 md:py-4 py-3 px-4',
         small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        round: base + ' px-2.5 py-1 md:px-3.5 md:py-2 text-sm',
         secondary: 'text-sm md:px-6 md:py-3.5 py-2.5 px-4 disabled:cursor-not-allowed border-2 border-stone-300 uppercase font-semibold text-stone-400 inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2',
     }
     if(to){
