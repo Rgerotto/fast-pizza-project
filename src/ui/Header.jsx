f2f0ef805a66ccbcfc38d7f2984e98b1265260a4
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-11-06 07:28:29 +0100

    change the name project

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,7 +5,7 @@ import Username from "../features/user/Username"
 function Header() {
     return (
         <header className="flex items-center justify-between bg-yellow-400 uppercase px-4 py-3 sm:px-6 border-b border-stone-200">
-            <Link to='/' className="tracking-widest">Fast React Pizza Co.</Link>
+            <Link to='/' className="tracking-widest">Pizzaria Project</Link>
             <SearchOrder />
             <Username />
         </header>
